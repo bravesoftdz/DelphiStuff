467dba8a3b15d7f8ee1bed2a1da94e50ecd765b7
yeahbah <yeahbah@outlook.com>
2013-10-18 13:19:26 -0600

    Thread cancelation example using DSharp.Core.Threading

---
+++ Threading/Project13.dpr
@@ -0,0 +1,13 @@
+program Project13;
+
+uses
+  FMX.Forms,
+  Unit25 in 'Unit25.pas' {Form25};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm25, Form25);
+  Application.Run;
+end.
---
+++ Threading/Unit25.pas
@@ -0,0 +1,144 @@
+unit Unit25;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants, 
+  FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
+  DSharp.Core.Threading, FMX.Edit, FMX.Layouts, FMX.Memo, Generics.Collections;
+
+type
+  TMyThread = class(TThread)
+  private
+    fTask: TTask;
+  public
+    procedure Execute; override;
+    procedure Cancel;
+  end;
+
+  TForm25 = class(TForm)
+    Button1: TButton;
+    Memo1: TMemo;
+    Button2: TButton;
+    Button3: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+  private
+    { Private declarations }
+    fMyThread: TMyThread;
+    fThreadList: TList<TMyThread>;
+  public
+    { Public declarations }
+  end;
+
+var
+  Form25: TForm25;
+
+implementation
+
+{$R *.fmx}
+
+procedure TForm25.Button1Click(Sender: TObject);
+begin
+  fMyThread := TMyThread.Create(true);
+  fMyThread.FreeOnTerminate := true;
+  fThreadList.Add(fMyThread);
+
+  fMyThread.Start;
+
+end;
+
+{ TMyThread }
+
+procedure TMyThread.Cancel;
+begin
+  if not fTask.Canceled then
+  begin
+    fTask.Cancel;
+    Synchronize(
+      procedure
+      begin
+        Form25.Memo1.Lines.Add(Self.ThreadID.ToString + ' is canceled');
+      end);
+  end;
+end;
+
+procedure TMyThread.Execute;
+begin
+  fTask := TTask.Create(
+    procedure
+    begin
+      Sleep(2000);
+    end);
+  fTask.Start;
+
+//  fTask.Wait;
+
+  Synchronize(
+    procedure
+    var
+      i, j: integer;
+    begin
+      i := -1;
+      while not fTask.Finished do
+      begin
+        if i > -1 then
+        begin
+          j := Length(Form25.Memo1.Lines[i]);
+          if Form25.Memo1.Lines[i].EndsWith('/') then
+            Form25.Memo1.Lines[i]:= Form25.Memo1.Lines[i].Replace('/', '\', [])
+          else
+            Form25.Memo1.Lines[i] := Form25.Memo1.Lines[i].Replace('\', '/', []);
+        end
+        else
+          i := Form25.Memo1.Lines.Add('Thread id: ' + Self.ThreadID.ToString + ' Inner task is running. /');
+
+        Sleep(10);
+
+        Application.ProcessMessages;
+        if fTask.Canceled then
+        begin
+          Form25.Memo1.Lines[i] := Form25.Memo1.Lines[i] + ' canceled';
+          break;
+        end;
+      end;
+
+      if not fTask.Canceled then
+      begin
+        if i > -1 then
+          Form25.Memo1.Lines[i] := Form25.Memo1.Lines[i] + ' done.'
+        else
+          Form25.Memo1.Lines.Add('Done ' + Self.ThreadID.ToString);
+        Application.ProcessMessages;
+      end;
+    end);
+end;
+
+procedure TForm25.Button2Click(Sender: TObject);
+var
+  t: TMyThread;
+begin
+  for t in fThreadList do
+    if t <> nil then
+      t.Cancel;
+end;
+
+procedure TForm25.Button3Click(Sender: TObject);
+begin
+  Memo1.Lines.Clear;
+end;
+
+procedure TForm25.FormCreate(Sender: TObject);
+begin
+  fThreadList := TList<TMyThread>.Create;
+end;
+
+procedure TForm25.FormDestroy(Sender: TObject);
+begin
+  fThreadList.Free;
+end;
+
+end.
